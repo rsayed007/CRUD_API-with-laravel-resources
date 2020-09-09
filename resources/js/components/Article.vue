aaa1caf72b543197ed610476b36e002d0dd36366
Syed Nayeem Roman <romansyed007@gmail.com>
2020-09-09 15:10:49 +0600

    fetch data from api with vuejs

---
+++ resources/js/components/Article.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <h2 class="text-center">Article</h2>
+        <div class=" card card-body mb-2" v-for="article in articles" v-bind:key="article.id">
+            <h3> {{article.title}} </h3>
+            <p> {{article.body}} </p>
+        </div>
+            <br>    
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                articles:[],
+                article: {
+                    id: '',
+                    title: '',
+                    body: ''
+                },
+                article_id: '',
+                pagination:{},
+                edit: false,
+            }
+        },
+        created(){
+            this.fetchArticles();
+        },
+        methods: {
+            fetchArticles(){
+                fetch('api/articles')
+                    .then(res => res.json())
+                    .then(res => {
+                        // console.log(res.data);
+                        this.articles = res.data;
+                    })
+            },
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark justify-content-between ">
+            <!-- Brand/logo -->
+            <a class="navbar-brand float-left" href="#">Logo</a>
+            
+            <!-- Links -->
+            <ul class="navbar-nav float-right">
+                <li class="nav-item">
+                <a class="nav-link" href="#">Link 1</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Link 2</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Link 3</a>
+                </li>
+            </ul>
+        </nav>
+        
+
+    </div>
+</template>
